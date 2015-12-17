4375a8c6afce0fb1d155ca589c7bc87d0c56576c
Erik Frederiksen <erik@noodlecake.com>
2015-12-17 13:44:53 -0600

    Day 04 six zeroes.

--- day04.py
+++ day04.py
@@ -6,11 +6,11 @@ def main(inputstr):
     while True:
         s = "%s%d" % (inputstr, i)
         checksum = hashlib.md5(s).hexdigest()
-        if checksum.startswith("00000"):
+        if checksum.startswith("000000"):
             print inputstr, i
             break
         i += 1
 
-main("abcdef")
-main("pqrstuv")
+# main("abcdef")
+# main("pqrstuv")
 main("ckczppom")
